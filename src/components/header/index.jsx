8169a4cbf8fa453b366661b7bcde4a769be05261
datchlive <71320906+DatchLive@users.noreply.github.com>
2021-03-21 15:26:46 +0900

    💄 ハンバーガーメニュー作成

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,32 +1,13 @@
-import MenuIcon from "@material-ui/icons/Menu";
 import Link from "next/link";
+import { NavBar } from "../navigation";
 
 export const Header = () => {
   return (
-    <header className="bg-green-400 text-white relative">
+    <header className="bg-green-700 text-white fixed w-full">
       <div className="text-center">
-        <h1 className="couragette text-4xl uppercase pt-8">Wise Life Log</h1>
+        <h1 className=" text-4xl uppercase pt-8">Wise Life Log</h1>
         <h2>〜賢く生きるための情報を発信！〜</h2>
-      </div>
-      <div>
-        <button className="absolute top-3 right-3 md:hidden">
-          <MenuIcon />
-        </button>
-      </div>
-
-      <div>
-        <ul className="uppercase md:flex md:justify-around">
-          <li>
-            <Link href="/">
-              <a className="">home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a className="">about</a>
-            </Link>
-          </li>
-        </ul>
+        <NavBar />
       </div>
     </header>
   );
