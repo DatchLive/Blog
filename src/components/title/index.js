b03b218ee8c5f1b109ca7086b2a2055e0be4124c
datchlive <71320906+DatchLive@users.noreply.github.com>
2021-04-03 12:16:51 +0900

    💄 フォント・カラー修正

--- src/components/title/index.js
+++ src/components/title/index.js
@@ -1,6 +1,6 @@
 export const Title = (props) => {
   return (
-    <h3 className="font-serif text-3xl text-center mb-4 uppercase">
+    <h3 className="font-courgette text-gray-900 text-3xl text-center mb-4 uppercase">
       {props.title}
     </h3>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,46 @@
 import Link from "next/link";
 import { Layout } from "../components/layout";
 import { Title } from "../components/title";
-// import { DateComponent } from "../components/date";
+import { DateComponent } from "../components/date";
 
 const Home = ({ blogs }) => {
+  console.log(blogs);
   return (
     <Layout>
+      <Title title="new articles" />
       <div>
-        <Title title="new articles" />
-
-        <div>
-          {blogs.map((blog) => (
-            <li key={blog.id}>
-              <Link href="/blogs/[id]" as={`/blogs/${blog.id}`}>
-                <a>
-                  <h2>{blog.title}</h2>
-                </a>
-              </Link>
-              {/* {blog.tags.map((tag) => (
-                <React.Fragment key={tag.id}>
-                  <span>{tag.name}</span>
-                </React.Fragment>
-              ))} */}
-            </li>
-          ))}
-        </div>
-
-        {/* {articles.map((article) => (
-          <div key={article.sys.id}>
+        {blogs.map((blog) => (
+          <div key={blog.id}>
             <div className="container max-w-xl m-auto items-center">
               <div className="w-full flex flex-col mb-8 px-3">
                 <div className="overflow-hidden bg-white rounded-lg shadow hover:shadow-raised hover:translateY-2px transition">
-                  <Link href="/blogs/[id]" as={`blogs/${blog.id}`}>
-                    <a>
-                      <img
-                        className="w-full"
-                        src={"https:" + article.fields.image.fields.file.url}
-                        alt=""
-                      />
-                      <div className="p-6 flex flex-col justify-between">
-                        <h3 className="font-medium text-gray-900 mb-4 leading-normal">
-                          {article.fields.title}
-                        </h3>
-                        <p className="inline-flex items-center">
-                          <DateComponent dateString={article.fields.date} />
-                        </p>
-                      </div>
-                    </a>
+                  <Link href="/blogs/[id]" as={`/blogs/${blog.id}`}>
+                    <div>
+                      <a>
+                        <img className="w-full" src={blog.image.url} alt="" />
+                        <div className="p-6 flex flex-col justify-between">
+                          <h2 className="font-plus font-medium text-gray-900 mb-4 leading-normal">
+                            {blog.title}
+                          </h2>
+                          <div>
+                            <p className="inline-flex items-center">
+                              <DateComponent dateString={blog.publishedAt} />
+                            </p>
+                            {blog.tags.map((tag) => (
+                              <div key={tag.id}>
+                                <span>{tag.name}</span>
+                              </div>
+                            ))}
+                          </div>
+                        </div>
+                      </a>
+                    </div>
                   </Link>
                 </div>
               </div>
             </div>
           </div>
-        ))} */}
+        ))}
       </div>
     </Layout>
   );
