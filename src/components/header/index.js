63f8a3a782bda29c5f47b23901c19754ae159ebb
datchlive <71320906+DatchLive@users.noreply.github.com>
2021-04-03 23:47:51 +0900

    💄 ヘッダー・ナビゲーションスタイリング

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,8 +4,10 @@ export const Header = () => {
   return (
     <header className="bg-pale-green text-white fixed top-0 w-full">
       <div className="text-center">
-        <h1 className=" font-courgette text-4xl uppercase pt-8">Wise Life Log</h1>
-        <h2 className="font-mono">〜賢く生きるための情報を発信！〜</h2>
+        <h1 className=" font-courgette text-4xl uppercase pt-8">
+          Wise Life Log
+        </h1>
+        <h2 className="font-mono text-sm">〜賢く生きるための情報を発信！〜</h2>
         <NavBar />
       </div>
     </header>
