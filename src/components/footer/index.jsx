625bedd945d7b4258fef58e65694b5eeb272a60f
datchlive <71320906+DatchLive@users.noreply.github.com>
2021-06-05 23:24:50 +0900

    🐛 import文削除

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,9 +1,3 @@
-import Link from 'next/link'
-import { Title } from 'src/components/title'
-import { Profile } from 'src/components/profile'
-import { SocialMedia } from 'src/components/socialmedia'
-import { BtnComponent } from 'src/components/btn'
-
 export const Footer = () => {
     return (
         <footer className="mt-4 text-center font-plus">
