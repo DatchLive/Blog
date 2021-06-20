0c9de965374b6c01e244524193d3c5b71aa9317a
datchlive <71320906+DatchLive@users.noreply.github.com>
2021-06-20 22:39:28 +0900

    📦 ESLint設定修正

--- src/components/socialmedia/index.jsx
+++ src/components/socialmedia/index.jsx
@@ -1,20 +1,20 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const SocialMedia = (props) => {
-    return (
-        <div className="text-lg">
-            <ul className="flex justify-center">
-                <li className="px-1">
-                    <a href={props.twitter}>
-                        <FontAwesomeIcon icon={["fab", "twitter"]} />
-                    </a>
-                </li>
-                <li className="px-1">
-                    <a href={props.instagram}>
-                        <FontAwesomeIcon icon={["fab", "instagram"]} />
-                    </a>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className="text-lg">
+      <ul className="flex justify-center">
+        <li className="px-1">
+          <a href={props.twitter}>
+            <FontAwesomeIcon icon={["fab", "twitter"]} />
+          </a>
+        </li>
+        <li className="px-1">
+          <a href={props.instagram}>
+            <FontAwesomeIcon icon={["fab", "instagram"]} />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,54 +1,54 @@
+import { useForm } from "react-hook-form";
 import { Layout } from "src/components/layout";
 import { Title } from "src/components/title";
-import { useForm } from "react-hook-form";
 
 export default function Contact() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({ reValidateMode: "onSubmit" });
-    const onSubmit = (data) => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ reValidateMode: "onSubmit" });
+  const onSubmit = (data) => console.log(data);
 
-    return (
-        <Layout>
-            <Title title="Contact" />
-            <div>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <span className="mr-3 font-plus">お名前</span>
-                    {errors?.name && (
-                        <span className="text-xs text-red-500">入力必須</span>
-                    )}
-                    <input
-                        {...register("name", { required: true })}
-                        className="block w-full px-1.5 py-2 mb-8 leading-tight text-gray-900 bg-white border border-gray-900 rounded appearance-none focus:outline-none focus:border-gray-900"
-                        type="text"
-                    />
-                    <span className="mr-3 font-plus">メールアドレス</span>
-                    {errors?.email && (
-                        <span className="text-xs text-red-500">入力必須</span>
-                    )}
-                    <input
-                        {...register("email", { required: true })}
-                        className="block w-full px-1.5 py-2 mb-8 leading-tight text-gray-900 bg-white border border-gray-900 rounded appearance-none focus:outline-none focus:border-gray-900"
-                        type="text"
-                    />
-                    <span className="mr-3 font-plus">お問い合わせ内容</span>
-                    {errors?.email && (
-                        <span className="text-xs text-red-500">入力必須</span>
-                    )}
-                    <textarea
-                        {...register("contents", { required: true })}
-                        className="block w-full h-48 px-1.5 py-2 mb-12 leading-tight text-gray-900 bg-white border border-gray-900 rounded appearance-none resize-none no-resize focus:outline-none focus:border-gray-900"
-                    />
-                    <div className="text-center">
-                        <input
-                            type="submit"
-                            className="px-3 py-1 text-gray-500 duration-300 bg-gray-200 rounded-md cursor-pointer hover:bg-blue-100 hover:text-blue-500"
-                        />
-                    </div>
-                </form>
-            </div>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Title title="Contact" />
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <span className="mr-3 font-plus">お名前</span>
+          {errors?.name && (
+            <span className="text-xs text-red-500">入力必須</span>
+          )}
+          <input
+            {...register("name", { required: true })}
+            className="block w-full px-1.5 py-2 mb-8 leading-tight text-gray-900 bg-white border border-gray-900 rounded appearance-none focus:outline-none focus:border-gray-900"
+            type="text"
+          />
+          <span className="mr-3 font-plus">メールアドレス</span>
+          {errors?.email && (
+            <span className="text-xs text-red-500">入力必須</span>
+          )}
+          <input
+            {...register("email", { required: true })}
+            className="block w-full px-1.5 py-2 mb-8 leading-tight text-gray-900 bg-white border border-gray-900 rounded appearance-none focus:outline-none focus:border-gray-900"
+            type="text"
+          />
+          <span className="mr-3 font-plus">お問い合わせ内容</span>
+          {errors?.email && (
+            <span className="text-xs text-red-500">入力必須</span>
+          )}
+          <textarea
+            {...register("contents", { required: true })}
+            className="block w-full h-48 px-1.5 py-2 mb-12 leading-tight text-gray-900 bg-white border border-gray-900 rounded appearance-none resize-none no-resize focus:outline-none focus:border-gray-900"
+          />
+          <div className="text-center">
+            <input
+              type="submit"
+              className="px-3 py-1 text-gray-500 duration-300 bg-gray-200 rounded-md cursor-pointer hover:bg-blue-100 hover:text-blue-500"
+            />
+          </div>
+        </form>
+      </div>
+    </Layout>
+  );
 }
